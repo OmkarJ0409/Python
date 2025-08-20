99ed7a59eba56bd5702dcf9c77335a942ee10565
Omkar Jagtap <109743729+OmkarJ0409@users.noreply.github.com>
2025-08-20 14:32:10 +0530

    Add files via upload

---
+++ Exercise/Exercise_04.py
@@ -0,0 +1,76 @@
+a = "Practice Questions of List in Python"
+
+print(a)
+# Output : Practice Questions of List in Python
+
+print(len(a))
+# Output : 36
+
+print(a[1:4])
+# Output : rac
+
+print(a[1:7])
+# Output : ractic
+
+print(a[3:])
+# Output : ctice Questions of List in Python
+
+print(a[:5])
+# Output : Pract
+
+print(a[4:17])
+# Output : tice Question
+
+print(a[-2:-5:-1])
+# Output : oht
+
+print(a[1:7:1])
+# Output : ractic
+
+print(a[1:7:2])
+# Output : rci
+
+print(a[-5:])
+# Output : ython
+
+print(a[:4])
+# Output : Prac
+
+print(a[-2:-5:-2])
+# Output : ot
+
+print(a[11:15])
+# Output : esti
+
+print(a[:])
+# Output : Practice Questions of List in Python
+
+print(a[::2])
+# Output : Patc usin fLs nPto
+
+print(a[-5:-1])
+# Output : ytho
+
+print(a[7:1:-1])
+# Output : ecitca
+
+print(a[3:-3])
+# Output : ctice Questions of List in Pyt
+
+print(a[30:40])
+# Output : Python
+
+print(a[17:-1])
+# Output : s of List in Pytho
+
+print(a[10::-1])
+# Output : uQ ecitcarP
+
+print(a[::-1])
+# Output : nohtyP ni tsiL fo snoitseuQ ecitcarP
+
+print(a[::3])
+# Output : PccQso  siPh
+
+print(a[-10::])
+# Output : in Python
